35731903887d7ae51ddbfef80f8c3b8704b65f99
Matias Onores <matiasonores@integracomex.com.ar>
2025-07-01 08:52:49 -0300

    Agrego el service de Persona

---
+++ src/main/java/app/todo/taskmanagement/service/PersonaService.java
@@ -0,0 +1,42 @@
+package app.todo.taskmanagement.service;
+
+import app.todo.taskmanagement.domain.Persona;
+import app.todo.taskmanagement.domain.PersonaRepository;
+import org.jspecify.annotations.Nullable;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(propagation = Propagation.REQUIRES_NEW)
+public class PersonaService {
+
+    private final PersonaRepository personaRepository;
+
+    PersonaService(PersonaRepository personaRepository) {
+        this.personaRepository = personaRepository;
+    }
+
+    public void createPersona(Integer dni, String apellido, String nombre, @Nullable Integer edad) {
+        var persona = new Persona();
+        persona.setDni(dni);
+        persona.setApellido(apellido);
+        persona.setNombre(nombre);
+        persona.setEdad(edad);
+        personaRepository.saveAndFlush(persona);
+    }
+
+    public void updatePersona(Persona persona) {
+        personaRepository.saveAndFlush(persona);
+    }
+
+    public List<Persona> list(Pageable pageable) {
+        return personaRepository.findAllBy(pageable).toList();
+    }
+    public void deletePersona(Long id) {
+        personaRepository.deleteById(id);
+    }
+}
